a4f33bcab84382f3c95299dcf60d860a83092e28
AksshayaSivakumar <aksshayapugal@gmail.com>
2024-07-17 01:15:17 +0530

    Getting Started with Mongo DB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize=require('sequelize');
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
+let _db;
 
-const sequelize=new Sequelize('node-complete', 'root', 'drTopper@1994',{
-  dialect:'mysql',
-  host:'localhost'
-});
+const mongoConnect=(callback)=>{
+  MongoClient.connect('mongodb+srv://Aksshaya:drTopper%4094@cluster0.vqnabpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(client=>{
+    console.log('connected!')
+    _db=client.db();
+    //console.log(_db)
+    callback();
+  })
+  .catch(err=>{
+    console.log(err);
+    throw err;
+  })
+}
 
-module.exports=sequelize;
+const getDb=()=>{
+  if(_db)
+  {
+    return _db;
+  }
+  throw 'No database found!'
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
+
